9fa1674ab5f499a696d27d774c0209efd43df432
rssmitra <it.dep.rssm@gmail.com>
2024-02-02 16:10:34 +0700

    update form eswl

--- application/modules/pelayanan/views/Pl_pelayanan/form_19.php
+++ application/modules/pelayanan/views/Pl_pelayanan/form_19.php
@@ -1,20 +1,20 @@
 <div style="text-align: center;" font-size:="" 18px;=""><b>LAPORAN TINDAKAN ESWL</b></div>
-<div style="text-align: left;"><b><br></b></div>
-<div style="text-align: left;"><b>Tanggal Tindakan&nbsp;</b>:&nbsp;</div>
-<div style="text-align: left;"><br></div>
-<div style="text-align: left;"><b>Jam Tindakan&nbsp;</b>:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; s/d</div>
-<div style="text-align: left;"><br></div>
-<div style="text-align: left;"><b>Diagnosa Pra Bedah </b>&nbsp;: &nbsp;</div>
-<div style="text-align: left;"><b><br></b></div>
-<div style="text-align: left;"><b>Diagnosa Pasca Bedah </b>&nbsp;: &nbsp;</div>
-<div style="text-align: left;"><b><br></b></div>
-<div style="text-align: left;"><b>Tindakan </b>&nbsp;: &nbsp;</div>
-<div style="text-align: left;"><b><br></b></div>
-<div style="text-align: left;"><b>Komplikasi </b>&nbsp;: &nbsp;</div>
-<div style="text-align: left;"><br></div>
-<div style="text-align: center;"><b>PROSEDUR TINDAKAN</b></div>
-<div style="text-align: left;"><br></div>
-<div style="text-align: left;">-</div>
-<div style="text-align: left;">-</div>
-<div style="text-align: left;">-</div>
-<div style="text-align: left;">-</div>
+<br></b>
+<b>Tanggal Tindakan&nbsp;</b>:&nbsp;
+<br>
+<b>Jam Tindakan&nbsp;</b>:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; s/d
+<br>
+<b>Diagnosa Pra Bedah </b>&nbsp;: &nbsp;
+<b><br></b>
+<b>Diagnosa Pasca Bedah </b>&nbsp;: &nbsp;
+<b><br></b>
+<b>Tindakan </b>&nbsp;: &nbsp;
+<b><br></b>
+<b>Komplikasi </b>&nbsp;: &nbsp;
+<br>
+<b>PROSEDUR TINDAKAN</b>
+<br>
+-
+<br>-
+<br>-
+<br>-
