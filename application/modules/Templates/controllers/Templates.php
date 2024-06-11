6060dcd370bbc11b5cefa3437dd4a29fdede3032
rssmitra <it.dep.rssm@gmail.com>
2024-06-11 15:53:35 +0700

    update templates

--- application/modules/Templates/controllers/Templates.php
+++ application/modules/Templates/controllers/Templates.php
@@ -1804,42 +1804,16 @@ class Templates extends MX_Controller {
                     </table>';
         }else{
             $tgl_pemeriksaan = ($data_pm->tgl_periksa != '')?$this->tanggal->formatDateTime($data_pm->tgl_periksa) : $this->tanggal->formatDateTime($data_pm->tgl_isihasil); 
-            // hardcode
-
-            // if(isset($_GET['num'])){
-            // 	if(isset($_GET['num']) AND $_GET['num']==1){
-	        //     		$pm_ = 472854; 
-			//             $no_mr = '00246856';
-			//             $nama_pasien = 'Devi Fitarti';
-			//             $umur = '28';
-			//             $jk_ = 'Perempuan';
-			//             $tgl_daftar = '22 Oktober 2021 - 10:05:15';
-			//             $tgl_periksa = '22 Oktober 2021 - 10:33:42';
-			//             $dokter_pengirim = 'Arief Indra Sanjaya,dr. Sp PK';
-	        //     }
-
-	        //     if(isset($_GET['num']) AND $_GET['num']==2){
-	        //     	$pm_ = 472852; 
-		    //         $no_mr = '00221948';
-		    //         $nama_pasien = 'Alfan Ali';
-		    //         $umur = '27';
-		    //         $jk_ = 'Laki- Laki';
-		    //         $tgl_daftar = '22 Oktober 2021 - 11:20:12';
-		    //         $tgl_periksa = '22 Oktober 2021 - 11:21:22';
-		    //         $dokter_pengirim = 'Arief Indra Sanjaya,dr. Sp PK';
-	        //     }
-
-            // }else{
-            	// form db
-	            $pm_ = $pm;
-	            $no_mr = $data->reg_data->no_mr;
-	            $nama_pasien = ucwords(strtolower($data->reg_data->nama_pasien));
-	            $umur = $data->reg_data->umur;
-	            $jk_ = $jk;
-	            $tgl_daftar = $this->tanggal->formatDateTime($data->reg_data->tgl_jam_masuk);
-	            $tgl_periksa = $tgl_pemeriksaan;
-	            $dokter_pengirim = $data->reg_data->nama_pegawai;
-            // }
+            
+            $pm_ = $pm;
+            $no_mr = $data->reg_data->no_mr;
+            $nama_pasien = ucwords(strtolower($data->reg_data->nama_pasien));
+            $umur = $data->reg_data->umur;
+            $jk_ = $jk;
+            $tgl_daftar = $this->tanggal->formatDateTime($data->reg_data->tgl_jam_masuk);
+            $tgl_periksa = $tgl_pemeriksaan;
+            $dokter_pengirim = $data->reg_data->nama_pegawai;
+          
 
             $html .= '<table align="left" cellpadding="0" cellspacing="0" border="0" style="font-size:36px">
                         <tr>
