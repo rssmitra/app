973066762069f90476efd7952323d2c547d7f9b4
rssmitra <it.dep.rssm@gmail.com>
2024-08-09 07:52:59 +0700

    update meta description

--- application/modules/login/views/login_view.php
+++ application/modules/login/views/login_view.php
@@ -2,11 +2,13 @@
 <html lang="en">
   <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta charset="utf-8" />
-    <title><?php echo COMP_SORT; ?> - Form Login</title>
-
-    <meta name="description" content="User login page" />
+    <meta charset="UTF-8">
+    <meta name="description" content="Smart Hospital System 4.0 adalah sistem rumah sakit yang sudah terintegrasi antar modulnya dan juga terintegrasi dengan BPJS seperti VClaim, ICare JKN, Antrian Online, MJKN, Pcare, Satu Sehat Kemenkes, dsb." />
+    <meta name="keywords" content="SIMRS, Smart Hospital, Bridging, BPJS">
+    <meta name="author" content="<?php echo COMP_LONG; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+    <title><?php echo COMP_SORT; ?> - Form Login</title>
 
     <!-- bootstrap & fontawesome -->
     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.css" />
