984b04ca8284177df73bfdcd04d12adbe1a2c434
IT Dept RSSM <it.dep.rssm@gmail.com>
2025-05-16 16:31:22 +0700

    update

--- ApiMerge/index.php
+++ ApiMerge/index.php
@@ -31,22 +31,41 @@ $pdf = new PDFMerger; // or use $pdf = new \PDFMerger; for Laravel
 
 $file_name_merge_emr = 'EMR-'.$_GET['noreg'];
 
+sscanf($_GET['date'], '%d-%d-%d', $y, $m, $d);
+// echo '<pre>';print_r($y);
+// echo '<pre>';print_r($m);
+// echo '<pre>';print_r($d);
+// exit;
+
+$file_grouper = '../uploaded/casemix/grouper/'.$y.'/'.$m.'/'.$d.'/'.$_GET['sep'].'.pdf';
+if(file_exists($file_grouper)){
+	$pdf->addPDF('../uploaded/casemix/grouper/'.$y.'/'.$m.'/'.$d.'/'.$_GET['sep'].'.pdf', 'all');	
+}
+
 foreach ($_GET as $key => $value) {
-	if ( !in_array($key, array('action','noreg','sep','tipe','month','year','no_mr'))) {
+	if ( !in_array($key, array('action','noreg','sep','tipe','month','year','no_mr','date',''))) {
 		// untuk file casemix
-		$pdf->addPDF('../uploaded/casemix/log/'.$_GET[$key].'', 'all');		
+		$split = explode('.', $value);
+		if(isset($split[1]) && $split[1] == 'pdf'){
+			// $pdf->addPDF('../uploaded/casemix/log/'.$_GET[$key].'', 'all');		
+			$pdf->addPDF('../uploaded/casemix/log/'.$_GET[$key].'', 'all');		
+		}
 		// $getData[] = $_GET[$key];
 	}
 }
 
+
+
+// echo '<pre>';print_r($filename);die;
 if($_GET['action']=='download'){
 	// casemix
-	$pdf->merge('file', 'sirs/app'.str_replace('..','',$filename).'/'.$_GET['tipe'].'/'.$_GET['sep'].'.pdf'); 
+	$pdf->merge('file', ''.str_replace('..','',$filename).'/'.$_GET['tipe'].'/'.$_GET['sep'].'.pdf'); 
 	// emr
-	$pdf->merge('file', 'sirs/app'.str_replace('..','',$filename_mr).'/'.$file_name_merge_emr.'.pdf');
+	// $pdf->merge('file', ''.str_replace('..','',$filename_mr).'/'.$file_name_merge_emr.'.pdf');
 }else{
 	$pdf->merge($_GET['action'], ''.$_GET['sep'].'.pdf'); 
 }
+// $pdf->merge('download', ''.str_replace('..','',$filename).'/'.$_GET['tipe'].'/'.$_GET['sep'].'.pdf'); 
 $pdf->merge('browser', ''.$_GET['sep'].'.pdf');
 
 ?>
